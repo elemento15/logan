6e6a11dc18bfb123d786bb864b48c875a92ba6c5
Luis Lomeli <luism.lomeli@gmail.com>
2017-09-03 15:32:14 -0600

    Changeg title and favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <!-- <meta name="csrf-token" content="{{ csrf_token() }}"> -->
 
-    <title>{{ config('app.name', 'Logan') }}</title>
+    <title>Logan - Proyectos</title>
 
     <!-- Styles -->
     <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
